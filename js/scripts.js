957cf2b2ab1f408e86f2151d94acc0ce0413b756
Chris Rudnicky and Mike Picker <mike@>
2018-10-18 14:37:33 -0700

    Add javascript logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
 
+    $("#formOne").submit(function(event){
+      event.preventDefault();
+      var name =$("#nme").val();
+      var q1 = $("input#hp_ww").val();
+      // var q2 = $("input#Inputfood").val();
+      // var q3 = $("input#Inputmusic").val();
+      // var q4 = $("#iceCream").val();
+      // var q5 = $("#iceCream").val();
 
+
+}
+      $("ul#namelist").append("<li>" + name + " " + "</li>");
+      $("ul#HP").append("<li>" +  + " " + "</li>");
+      $("ul#WW").append("<li>" +  + " " + "</li>");
+      $("ul#DT").append("<li>" +  + " " + "</li>");
+      $("ul#JB").append("<li>" +  + " " + "</li>");
+    });
 });
+
+(q1 === yes )
